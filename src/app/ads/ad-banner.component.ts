111c48f9b43c22f990410178eb4d8f9c3fdc8f07
Lukas Matta <lukas@matta.sk>
2022-06-22 15:44:09 +0200

    fix: Update repo to reproduce import issue

---
+++ src/app/ads/ad-banner.component.ts
@@ -0,0 +1,56 @@
+import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+
+import { AdDirective } from './';
+import { AdItem } from './';
+import { AdComponent } from './';
+
+@Component({
+  selector: 'app-ad-banner',
+  template: `
+    <div class="ad-banner-example">
+      <h3>Advertisements</h3>
+      <ng-template adHost></ng-template>
+    </div>
+  `
+})
+export class AdBannerComponent implements OnInit, OnDestroy {
+  @Input() ads: AdItem[] = [];
+
+  currentAdIndex = -1;
+
+  @ViewChild(AdDirective, {static: true}) adHost!: AdDirective;
+  interval: number|undefined;
+
+  ngOnInit(): void {
+    this.loadComponent();
+    this.getAds();
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
+
+  loadComponent() {
+    this.currentAdIndex = (this.currentAdIndex + 1) % this.ads.length;
+    const adItem = this.ads[this.currentAdIndex];
+
+    const viewContainerRef = this.adHost.viewContainerRef;
+    viewContainerRef.clear();
+
+    const componentRef = viewContainerRef.createComponent<AdComponent>(adItem.component);
+    componentRef.instance.data = adItem.data;
+  }
+
+  getAds() {
+    this.interval = setInterval(() => {
+      this.loadComponent();
+    }, 3000);
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { HeroJobAdComponent } from './ads/hero-job-ad.component';
+import { AdBannerComponent } from './ads/ad-banner.component';
+import { HeroProfileComponent } from './ads/hero-profile.component';
+import { AdDirective } from './ads';
+import { AdService } from './ads';
 
 @NgModule({
+  imports: [ BrowserModule ],
+  providers: [ AdService ],
   declarations: [
-    AppComponent
+    AppComponent,
+    AdBannerComponent,
+    HeroJobAdComponent,
+    HeroProfileComponent,
+    AdDirective
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
